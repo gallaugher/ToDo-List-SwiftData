fe3f0a623af76c3c9ace2f7e38094031d3c05c7a
gallaugher <john.gallaugher@bc.edu>
2024-09-01 19:23:55 -0400

    Initial Commit

---
+++ ToDo-List-SwiftData/ContentView.swift
@@ -0,0 +1,21 @@
+// File: ContentView.swift Project: ToDo-List-SwiftData
+// Created by: Prof. John Gallaugher on 9/1/24
+// YouTube.com/profgallaugher  -  threads.net/john.gallaugher
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ToDo-List-SwiftData/ToDo_List_SwiftDataApp.swift
@@ -0,0 +1,14 @@
+// File: ToDo_List_SwiftDataApp.swift Project: ToDo-List-SwiftData
+// Created by: Prof. John Gallaugher on 9/1/24
+// YouTube.com/profgallaugher  -  threads.net/john.gallaugher
+
+import SwiftUI
+
+@main
+struct ToDo_List_SwiftDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
